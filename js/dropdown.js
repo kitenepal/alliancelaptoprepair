2ff5d90caa8b6e3d7bc7a8bbea8444fb64da42cd
NidanStha <nidanshrestha@gmail.com>
2018-06-07 14:34:44 +0545

    hover dropdown

--- js/dropdown.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $('.dropdown-submenu a.test').on("mouseover", function(e){
-    $(this).next('ul').toggle();
-    e.stopPropagation();
-    e.preventDefault();
-  });
-});
